2a700afb72ae3829daa8e613ec5665577de8d16b
shaogz1077 <shaogz1077@126.com>
2015-10-19 21:59:04 +0800

    添加了自动构建UI的jar库

---
+++ app/src/main/java/com/gameinformation/fun/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gameinformation.fun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.gameinformation.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
